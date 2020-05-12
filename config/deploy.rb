e18aa27a9d62c51e053da4b3ea9baee6c6af874f
charllesfaltesia <hahaharogen@yahoo.co.jp>
2020-05-12 19:55:13 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.0"
 
 set :application, "flower-knight"
-set :repo_url, "git@example.com:charllesfaltesia/flower-knight.git"
+set :repo_url, "git@github.com:charllesfaltesia/flower-knight.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
